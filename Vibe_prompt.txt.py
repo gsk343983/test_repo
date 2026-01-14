526bcc24d7952939b73fedb607dc01e8ad006cf3
Muskan Sharma <73130447+Micacegle09@users.noreply.github.com>
2026-01-14 14:15:19 +0530

    Update Vibe_prompt.txt.py

--- Vibe_prompt.txt.py
+++ Vibe_prompt.txt.py
@@ -1,12 +1,3 @@
 # Python testing file
 # Updated the file after taking clone in the local
 print("Hello World")
-<<<<<<< Updated upstream
-print("Only available in remote repo - Done by team member 1")
-=======
-<<<<<<< HEAD
-print("Only available in remote repo - Done by team member 1")
-=======
-print("Only available in remote repo - Done by team member 1")
->>>>>>> 12f63474482aec87dee9128407ccd33fc31eac93
->>>>>>> Stashed changes
